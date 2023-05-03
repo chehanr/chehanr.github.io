b9d405bb8451acd8f883d0dda9b0c004eac3719c
Chehan Ratnasiri <chehan.rat@gmail.com>
2023-05-03 20:55:09 +0530

    feat: use server output bc error

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,13 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import image from "@astrojs/image";
-
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://chehanr.com',
   integrations: [mdx(), sitemap(), tailwind(), react(), image()],
-  output: "static",
+  output: "server",
   adapter: vercel({
     analytics: true
   })
