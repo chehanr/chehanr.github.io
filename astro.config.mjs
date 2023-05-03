d3322bf23f4e725e3278a89d6e70dd492d004376
Chehan Ratnasiri <chehan.rat@gmail.com>
2023-05-03 20:51:13 +0530

    feat: add vercel integration with analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,14 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import image from "@astrojs/image";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://chehanr.com',
-  integrations: [mdx(), sitemap(), tailwind(), react(), image()]
+  integrations: [mdx(), sitemap(), tailwind(), react(), image()],
+  output: "static",
+  adapter: vercel({
+    analytics: true
+  })
 });
