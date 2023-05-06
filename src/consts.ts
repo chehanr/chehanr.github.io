25608b8c5ec5af7319f31eda254e44f8dd8650c2
Chehan Ratnasiri <chehan.rat@gmail.com>
2023-05-06 20:53:12 +0530

    feat: typed themes

--- src/consts.ts
+++ src/consts.ts
@@ -4,3 +4,4 @@
 export const SITE_TITLE = "Chehan Ratnasiri";
 export const SITE_DESCRIPTION = "Chehan's personal webspace";
 export const GOOGLE_TAG_MEASUREMENT_ID = "G-8WZY4CQBDH";
+export const THEMES = ["light", "dark"] as const;
---
+++ src/types.ts
@@ -0,0 +1,3 @@
+import type { THEMES } from "./consts";
+
+export type Theme = (typeof THEMES)[number];
